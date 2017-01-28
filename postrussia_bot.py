1941aaa172194fa971bf7feac6b7f89e139c6f97
LAPTOP-0TGQGA3T\tayak <taya.kulagina@gmail.com>
2017-01-28 15:50:13 +0300

    Add /info

--- postrussia_bot.py
+++ postrussia_bot.py
@@ -1,22 +1,36 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
-from redis import StrictRedis
-r = StrictRedis()
+import redis
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # 48826383
 
 def start(bot, update):
-	print("Вызван /start")
-	bot.sendMessage(update.message.chat_id, text="Welcome to the friendliest service ever - Post of Russia!")
+    print("Вызван /start")
+    bot.sendMessage(update.message.chat_id, text="Welcome to the friendliest service ever - Post of Russia!")
+
+def info(bot, update):
+    print('Пользователь интересуется, а что это он там ждет от Почты России')
+    chat_id = 'chat_id:'+ str(update.message.chat_id)
+    pochta_info = r.get(chat_id)
+    pochta_info = pochta_info.decode('utf-8').split(',')
+    print(pochta_info)
+    pochta_result = ''
+    for i, element in enumerate(pochta_info):
+        pochta_result += ('{}. {}'.format(i+1, element))
+        pochta_result += '\n'
+    bot.sendMessage(update.message.chat_id, text=pochta_result)
+
 
 def add(bot, update):
+    print('Добавление почтового отправления')
     pochta_add = update.message.text
     pochta_add = pochta_add.split()
     pochta_add = pochta_add[1]
-    
     chat_id = 'chat_id:'+ str(update.message.chat_id)
     user_code = r.get(chat_id)
     if user_code is None:
         get_pochta = r.set(chat_id, str(pochta_add))
+        print(get_pochta)
     else:
         user_code = user_code.decode('utf-8').split(',')
         if pochta_add in user_code:
@@ -25,6 +39,7 @@ def add(bot, update):
         user_code.append(pochta_add)
         user_code = ','.join(user_code)
         set_pochta = r.set(chat_id, user_code)
+    bot.sendMessage(update.message.chat_id, text='Всё добавили, теперь будем следить')
 
 def get(bot, update):
     print(update.message.text)
@@ -35,8 +50,7 @@ def get(bot, update):
     if user_data is None:
         bot.sendMessage(update.message.chat_id, text='nicho net')
         return
-
-    list_of_user_codes = user_data.decode('utf-8').split(',')  # [23, 77, 59]
+    list_of_user_codes = user_data.decode('utf-8').split(',')
     print(list_of_user_codes)
     bot.sendMessage(update.message.chat_id, text=list_of_user_codes[int(tracking_number) - 1])
 
@@ -48,11 +62,10 @@ def run_bot():
 	dp.add_handler(CommandHandler("start", start))
 	dp.add_handler(CommandHandler("add", add))
 	dp.add_handler(CommandHandler("get", get))
+    dp.add_handler(CommandHandler("info", add))
 
 	updater.start_polling()
 	updater.idle()
 
-
-
 if __name__ == "__main__":
-	run_bot()
+    run_bot()
