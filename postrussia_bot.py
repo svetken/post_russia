d7ea9dc7a28e053a591407f8175a2ca124b36bca
LAPTOP-0TGQGA3T\tayak <taya.kulagina@gmail.com>
2017-02-04 16:53:31 +0300

    Добавлена функция вызова API к Почте России

--- postrussia_bot.py
+++ postrussia_bot.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 import redis
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+from suds.client import Client
+
 
 # 48826383
 
@@ -19,6 +21,50 @@ def info(bot, update):
         pochta_result += ('{}. {}'.format(i+1, element))
         pochta_result += '\n'
     bot.sendMessage(update.message.chat_id, text=pochta_result)
+
+
+def check(bot, update):
+    print('Юзер отправляет запрос в Почту России. Боже, сохрани его!')
+    bot.sendMessage(update.message.chat_id, text="ну попробуй это прочитать!)))")
+    user_barcode = update.message.text.split(" ")[1]
+    print(user_barcode)
+    url = 'https://tracking.russianpost.ru/rtm34?wsdl'
+    client = Client(url, retxml=True, headers={'Content-Type': 'application/soap+xml; charset=utf-8'}, location='https://tracking.russianpost.ru/rtm34/Service.svc')
+    barcode = user_barcode
+    print(barcode)
+    my_login = 'PkFVrNfQbrLZSv'
+    my_password = 'RxAkvP1AKeGH'
+    print(my_password)
+    message = \
+"""<?xml version="1.0" encoding="UTF-8"?>
+                <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:oper="http://russianpost.org/operationhistory" xmlns:data="http://russianpost.org/operationhistory/data" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+   <soap:Header/>
+   <soap:Body>
+      <oper:getOperationHistory>
+         <!--Optional:-->
+         <data:OperationHistoryRequest>
+            <data:Barcode>""" + barcode+ """</data:Barcode>
+            <data:MessageType>0</data:MessageType>
+            <!--Optional:-->
+            <data:Language>RUS</data:Language>
+         </data:OperationHistoryRequest>
+         <!--Optional:-->
+         <data:AuthorizationHeader soapenv:mustUnderstand="?">
+            <data:login>"""+ my_login +"""</data:login>
+            <data:password>""" + my_password + """</data:password>
+         </data:AuthorizationHeader>
+      </oper:getOperationHistory>
+   </soap:Body>
+</soap:Envelope>"""
+    result = client.service.getOperationHistory(__inject={'msg':message})
+    result = result.decode('utf-8')
+    print(result)
+    sFile = open ("otv.txt",'w')
+    sFile.write(str(result))
+    sFile.close()
+    bot.sendMessage(update.message.chat_id, text=result)
+    #except Exception as e:
+    #   print(e)
 
 
 def add(bot, update):
@@ -56,16 +102,16 @@ def get(bot, update):
 
 
 def run_bot():
-	updater = Updater("302077885:AAEEA-WmwC-2aMfl2ozxr6rsN0lRKnOkiUw")
+    updater = Updater("302077885:AAEEA-WmwC-2aMfl2ozxr6rsN0lRKnOkiUw")
 
-	dp = updater.dispatcher
-	dp.add_handler(CommandHandler("start", start))
-	dp.add_handler(CommandHandler("add", add))
-	dp.add_handler(CommandHandler("get", get))
-    dp.add_handler(CommandHandler("info", add))
-
-	updater.start_polling()
-	updater.idle()
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("add", add))
+    dp.add_handler(CommandHandler("get", get))
+    dp.add_handler(CommandHandler("info", info))
+    dp.add_handler(CommandHandler("check", check))
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     run_bot()
