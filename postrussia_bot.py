03e66acd469100d83aebb71a999f05c900f90e57
Sveta Nov <svetkenov@gmail.com>
2017-02-11 17:20:22 +0300

    пофиксенный delete и логирование ошибок

--- postrussia_bot.py
+++ postrussia_bot.py
@@ -1,5 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 import redis
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
+log = logging.getLogger()
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # 48826383
@@ -25,6 +28,10 @@ def info(bot, update):
 def add(bot, update):
     print('Добавление почтового отправления')
     tracking_number = update.message.text.split()[1]
+    # data = get_data_from_pochta(tracking_number)
+    # data = parse_data_from_pochta(data)
+    # save_to_redis(data)
+
     chat_id = 'chat_id:'+ str(update.message.chat_id)
     list_of_parcels_str = r.get(chat_id)
     if list_of_parcels_str is None:
@@ -62,18 +69,23 @@ def info(bot, update):
     bot.sendMessage(update.message.chat_id, text=pochta_result)
 
 def delete(bot, update):
+    print('Пользователь хочет удалить из списка посылок')
     chat_id = 'chat_id:'+ str(update.message.chat_id)
     list_of_parcels = r.get(chat_id)
-    list_of_parcels.split(' ')
-    bot.sendMessage(update.message.chat_id, text='Here is your current list: ' + ', '.join(list_of_parcels))
+    list_of_parcels = list_of_parcels.decode('utf-8').split(',')
+   
 
-    what_to_del = input('Which one do you wanna delete? ')
+    what_to_del = update.message.text.split()[1]
     if int(what_to_del) > len(list_of_parcels):
         bot.sendMessage(update.message.chat_id, text='wtf are you doing?')
     elif int(what_to_del) <= 0:
         bot.sendMessage(update.message.chat_id, text='Nothing to delete')
     else:
         del list_of_parcels[int(what_to_del) - 1]
+        r.set(chat_id, ','.join(list_of_parcels))
+
+    print('Удалён элемент из списка посылок')
+
     
     text = 'Okay. Thats what you have for now: '
 
@@ -86,19 +98,18 @@ def delete(bot, update):
 
 
 def run_bot():
-	updater = Updater("302077885:AAEEA-WmwC-2aMfl2ozxr6rsN0lRKnOkiUw")
+    updater = Updater("302077885:AAEEA-WmwC-2aMfl2ozxr6rsN0lRKnOkiUw")
 
-	dp = updater.dispatcher
-	dp.add_handler(CommandHandler("start", start))
-	dp.add_handler(CommandHandler("add", add))
-	dp.add_handler(CommandHandler("get", get))
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("add", add))
+    dp.add_handler(CommandHandler("get", get))
     dp.add_handler(CommandHandler("info", info))
     dp.add_handler(CommandHandler("delete", delete))
     dp.add_handler(CommandHandler("info", add))
->>>>>>> c6d719952abd48900d05d9fd8910cb2872a42029
 
-	updater.start_polling()
-	updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     run_bot()
